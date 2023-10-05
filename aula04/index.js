8cd6d0ac454b10b0f976cd1307768ed4bb7776b1
gui :) <136282197+guixlzzz@users.noreply.github.com>
2023-10-05 11:08:08 -0300

    up

---
+++ aula04/index.js
@@ -0,0 +1,13 @@
+var date = new Date()
+var diasem = date.getDay()
+switch(diasem) {
+    case 0: console.log('Hoje é domingo')
+    break
+    case 1: console.log('Hoje pé segunda')
+    break
+    case 2: console.log('hoe é terça')
+    break
+    case 3: console.log('hoje é quarta')
+    case 4: console.log('hoje é quinta')
+    break
+}
