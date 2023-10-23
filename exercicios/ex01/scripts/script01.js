aee8934f6c86f123d087d29ff2ea7f31872980eb
gui :) <136282197+guixlzzz@users.noreply.github.com>
2023-10-23 15:53:57 -0300

    up

---
+++ exercicios/ex01/scripts/script01.js
@@ -0,0 +1,23 @@
+
+function carregar() {
+    var msg = document.getElementById('msg');
+    var img = document.getElementById('imagem');
+    var data = new Date();
+    //var hora = data.getHours();
+    var mnt = data.getMinutes()
+    var s = data.getSeconds()
+    var hora = 18
+    msg.innerHTML = `Agora são ${hora} horas e ${mnt} minutos e ${s} segundos.`;
+    if (hora > 5 && hora < 12) {
+        img.src = 'imagem/tarde.png';
+        document.body.style.background = '#f1f12add';
+    }
+    else if (hora >= 12 && hora < 17) {
+        img.src = 'imagem/manha.png';
+        document.body.style.background = '#f9b22d';
+    }
+    else {
+        img.src = 'imagem/nooite.png';
+        document.body.style.background = '#7d217d72';
+    }
+}
